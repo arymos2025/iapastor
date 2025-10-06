21ca94c25a4f34e5a0edd95c0780bc8205591cdf
arymos2025 <aruxanr@gmail.com>
2025-10-06 17:07:17 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ from langchain_core.runnables import RunnablePassthrough
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.documents import Document
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain_community.embeddings import HuggingFaceEmbeddings # <--- IMPORTACIÓN DEL MODELO GRATUITO
 from gtts import gTTS
 import tempfile
 
